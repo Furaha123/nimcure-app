d4f38ffd4632e2c9b0403bca009117b21ca2fb2c
mosesfuraha <moses.furaha@amalitechtraining.org>
2025-02-17 02:23:11 +0200

    feat: Add progressive tabs for the assiging package

---
+++ app/assign-package/page.tsx
@@ -0,0 +1,167 @@
+import React, { useState } from "react";
+
+const AssignPackage = () => {
+  const [selectedTab, setSelectedTab] = useState("drugCycle");
+  const [selectedCycle, setSelectedCycle] = useState<string | null>(null);
+
+  const handleContainerClick = (cycleId: string) => {
+    setSelectedCycle(selectedCycle === cycleId ? null : cycleId);
+  };
+
+  return (
+    <div className="flex min-h-screen mt-12">
+      <div className="w-1/2">
+        <div className="bg-[#ffffff] w-[90%] p-6 h-[40%]">
+          <h2 className="text-lg font-medium border-b pb-4 mb-6">
+            Patient Information
+          </h2>
+
+          <div className="space-y-4 p-6">
+            <div className="flex justify-between">
+              <label className="text-[#2A2A2AB2] text-sm">Hospital ID</label>
+              <span className="font-bold text-[#2A2A2A]">1AFHFH093</span>
+            </div>
+
+            <div className="flex justify-between">
+              <label className="text-gray-500 text-sm">Name</label>
+              <span className="font-bold text-[#2A2A2A]">
+                Oluwaseun Aregbesola
+              </span>
+            </div>
+
+            <div className="flex justify-between">
+              <label className="text-gray-500 text-sm">Phone Number:</label>
+              <span className="font-bold text-[#2A2A2A]">+2347068642920</span>
+            </div>
+
+            <div className="flex justify-between">
+              <label className="text-gray-500 text-sm">
+                Next Delivery Date:
+              </label>
+              <span className="font-bold text-[#2A2A2A]">
+                12th September 2020
+              </span>
+            </div>
+
+            <div className="flex justify-between">
+              <label className="text-gray-500 text-sm">Location:</label>
+              <span className="font-bold text-[#2A2A2A]">Yaba, Lagos</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full">
+        <div className="bg-[#ffffff] p-6">
+          <div className="mb-8">
+            <div className="flex justify-around relative">
+              {[
+                { id: "drugCycle", label: "Set Drug Cycle/Length" },
+                { id: "dispatchRider", label: "Assign Dispatch Rider" },
+                { id: "scanPackage", label: "Scan Package" },
+              ].map((tab) => (
+                <div
+                  key={tab.id}
+                  className={`flex items-center space-x-2 pb-6 cursor-pointer ${
+                    selectedTab === tab.id ? "border-b-4 border-blue-600" : ""
+                  }`}
+                  onClick={() => setSelectedTab(tab.id)}
+                >
+                  <input
+                    type="radio"
+                    name="formType"
+                    id={tab.id}
+                    className="w-4 h-4 text-blue-500 accent-blue-500"
+                    checked={selectedTab === tab.id}
+                    onChange={() => setSelectedTab(tab.id)}
+                  />
+                  <label
+                    htmlFor={tab.id}
+                    className={`font-medium cursor-pointer ${
+                      selectedTab === tab.id ? "text-blue-500" : "text-gray-500"
+                    }`}
+                  >
+                    {tab.label}
+                  </label>
+                </div>
+              ))}
+            </div>
+
+            <div className="border-b mt-[-1px]"></div>
+          </div>
+
+          <div>
+            <p className="text-gray-700 mb-6 font-bold">
+              Oluwaseun Aregbesola has a drug cycle of two(2) months
+            </p>
+
+            <div
+              className={`border border-2 p-4 space-y-4 mb-4 cursor-pointer hover:bg-gray-50 transition-all duration-200 ${
+                selectedCycle === "sameCycle"
+                  ? "border-blue-500"
+                  : "border-gray-200"
+              }`}
+              onClick={() => handleContainerClick("sameCycle")}
+            >
+              <div
+                className={`flex items-center space-x-3 pb-3 border-b ${
+                  selectedCycle === "sameCycle"
+                    ? "border-blue-500"
+                    : "border-gray-300"
+                }`}
+              >
+                <input
+                  type="radio"
+                  name="cycle"
+                  id="sameCycle"
+                  className="w-6 h-6 text-blue-500 accent-blue-500 transition-transform duration-300 ease-in-out transform scale-100 checked:scale-110"
+                  checked={selectedCycle === "sameCycle"}
+                  onChange={() => handleContainerClick("sameCycle")}
+                />
+                <label
+                  htmlFor="sameCycle"
+                  className={`text-lg transition-colors duration-300 ${
+                    selectedCycle === "sameCycle"
+                      ? "text-blue-500 font-bold"
+                      : "text-gray-700"
+                  }`}
+                >
+                  Same as initial drug cycle
+                </label>
+              </div>
+
+              <p className="pl-7 transition-colors duration-300">
+                Deliver drug on{" "}
+                <span className="font-bold">4th February 2020</span> & set next
+                delivery date to{" "}
+                <span className="font-bold">4th March 2020</span>
+              </p>
+            </div>
+
+            <div className="border  p-4 space-y-4 mb-4 cursor-pointer hover:bg-gray-50 transition-all duration-200">
+              <div className="flex items-center space-x-3">
+                <input
+                  type="radio"
+                  name="cycle"
+                  id="newCycle"
+                  className="w-6 h-6 text-blue-500 accent-blue-500 transition-transform duration-300 ease-in-out transform scale-100 checked:scale-110"
+                />
+                <label htmlFor="newCycle" className="text-gray-700">
+                  Set new drug cycle
+                </label>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex justify-end mt-8">
+            <button className="bg-blue-400 text-white px-8 py-2 rounded-lg hover:bg-blue-500 transition-colors">
+              Next
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AssignPackage;
--- app/patient-details/[hospitalId]/page.tsx
+++ app/patient-details/[hospitalId]/page.tsx
@@ -6,11 +6,15 @@ import { patients } from "@/app/data";
 import { Patient } from "@/types";
 import Layout from "@/app/layout/page";
 import { CustomButton } from "@/components";
+import AssignPackage from "@/app/assign-package/page";
 
 const PatientDetails = () => {
   const [patient, setPatient] = useState<Patient | null>(null);
   const [firstName, setFirstName] = useState<string>("");
   const [lastName, setLastName] = useState<string>("");
+  const [showAssignPackage, setShowAssignPackage] = useState(false);
+
+  const [activeTab, setActiveTab] = useState<"patient" | "delivery">("patient");
   const params = useParams();
   const hospitalId = params?.hospitalId as string;
 
@@ -32,6 +36,12 @@ const PatientDetails = () => {
     }
   }, [hospitalId]);
 
+  const handleNavigationClick = (section: string) => {
+    if (section === "viewPatients") {
+      setShowAssignPackage(false);
+    }
+  };
+
   const getStatusColor = (status: string): string => {
     const formattedStatus = status.toLowerCase();
     switch (formattedStatus) {
@@ -48,6 +58,157 @@ const PatientDetails = () => {
     }
   };
 
+  const renderPatientForm = () => (
+    <form className="w-full lg:w-2/3 grid grid-cols-1 gap-6 p-4">
+      <div className="border-[2px] border-[#CFCFCF] bg-[#EFEFEF] p-2">
+        <label className="text-sm">Hospital ID</label>
+        <input
+          type="text"
+          className="w-full border-none outline-none mt-1 bg-transparent font-gilroy font-bold"
+          value={patient?.hospital_id}
+          readOnly
+        />
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+        <div className="border-[2px] border-[#CFCFCF] bg-[#EFEFEF] p-2">
+          <label className="text-sm text-gray-500">First Name</label>
+          <input
+            type="text"
+            className="w-full border-none outline-none mt-1 bg-transparent font-gilroy font-bold"
+            value={firstName}
+            onChange={(e) => {
+              setFirstName(e.target.value);
+              if (patient) {
+                const updatedPatient = {
+                  ...patient,
+                  patient_name: `${e.target.value} ${lastName}`.trim(),
+                };
+                setPatient(updatedPatient);
+              }
+            }}
+          />
+        </div>
+
+        <div className="border-[2px] border-[#CFCFCF] bg-[#EFEFEF] p-2">
+          <label className="text-sm text-gray-500">Last Name</label>
+          <input
+            type="text"
+            className="w-full border-none outline-none mt-1 bg-transparent font-gilroy font-bold"
+            value={lastName}
+            onChange={(e) => {
+              setLastName(e.target.value);
+              if (patient) {
+                const updatedPatient = {
+                  ...patient,
+                  patient_name: `${firstName} ${e.target.value}`.trim(),
+                };
+                setPatient(updatedPatient);
+              }
+            }}
+          />
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+        <div className="border-[2px] border-[#CFCFCF] bg-[#EFEFEF] p-2">
+          <label className="text-sm text-gray-500">Gender</label>
+          <select className="w-full border-none outline-none mt-1 bg-transparent font-gilroy font-bold">
+            <option value="Male" selected={patient?.gender === "Male"}>
+              Male
+            </option>
+            <option value="Female" selected={patient?.gender === "Female"}>
+              Female
+            </option>
+          </select>
+        </div>
+
+        <div className="border-[2px] border-[#CFCFCF] bg-[#EFEFEF] p-2">
+          <label className="text-sm text-gray-500">Phone Number</label>
+          <input
+            type="text"
+            className="w-full border-none outline-none mt-1 bg-transparent font-gilroy font-bold"
+            value={patient?.phone_number}
+          />
+        </div>
+      </div>
+
+      <div className="border-[2px] border-[#CFCFCF] bg-[#EFEFEF] p-2">
+        <label className="text-sm text-gray-500">Email Address</label>
+        <input
+          type="email"
+          className="w-full border-none outline-none mt-1 bg-transparent font-gilroy font-bold"
+          value={patient?.email}
+        />
+      </div>
+    </form>
+  );
+
+  const renderDeliveryForm = () => (
+    <form className="w-full lg:w-2/3 grid grid-cols-1 gap-6 p-4">
+      <div className="grid grid-cols-1 lg:grid-cols-1 gap-6">
+        <div className="border-[2px] border-[#CFCFCF] bg-[#EFEFEF] p-2">
+          <label className="text-sm text-gray-500">Delivery Date</label>
+          <input
+            type="text"
+            className="w-full border-none outline-none mt-1 bg-transparent font-gilroy font-bold"
+            value="2024-02-16"
+          />
+        </div>
+      </div>
+      <div className="border-[2px] border-[#CFCFCF] bg-[#EFEFEF] p-2">
+        <label className="text-sm text-gray-500">Delivery Area</label>
+        <input
+          type="text"
+          className="w-full border-none outline-none mt-1 bg-transparent font-gilroy font-bold"
+          value="Yaba Main St, City"
+        />
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-1 gap-6">
+        <div className="border-[2px] border-[#CFCFCF] bg-[#EFEFEF] p-2">
+          <label className="text-sm text-gray-500">Delivery Address</label>
+          <input
+            type="text"
+            className="w-full border-none outline-none mt-1 bg-transparent font-gilroy font-bold"
+            value="123 Main St, City"
+          />
+        </div>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-1 gap-6">
+        <div className="border-[2px] border-[#CFCFCF] bg-[#EFEFEF] p-2">
+          <label className="text-sm text-gray-500">Payment Status</label>
+          <input
+            type="text"
+            className="w-full border-none outline-none mt-1 bg-transparent font-gilroy font-bold"
+            value={patient?.status || ""}
+            readOnly
+          />
+        </div>
+      </div>
+    </form>
+  );
+  const renderBreadcrumb = () => (
+    <h2 className="text-2xl font-gilroy font-semibold mb-6 text-center sm:text-left">
+      {showAssignPackage ? (
+        <>
+          <span className="text-sm text-[#276DF7]">Patients </span>
+          <span
+            className="text-sm text-[#276DF7] cursor-pointer"
+            onClick={() => handleNavigationClick("viewPatients")}
+          >
+            / View Patients /{" "}
+          </span>
+          <span className="text-lg font-bold">Assign Package</span>
+        </>
+      ) : (
+        <>
+          <span className="text-sm text-[#276DF7]">Patients </span>
+          <span className="text-lg font-bold ">/ View Patients</span>
+        </>
+      )}
+    </h2>
+  );
   if (!patient) {
     return (
       <>
@@ -67,181 +228,113 @@ const PatientDetails = () => {
   }
 
   return (
-    <>
+    <div className="bg-[#f9f9f9]">
       <Layout />
       <div className="max-w-screen-2xl mx-auto mt-6 mb-4 font-gilroy">
-        <div className="bg-white">
+        <div className="">
           <div className="flex flex-col sm:flex-row justify-between items-center border-b gap-4">
-            <h2 className="text-2xl font-gilroy font-semibold mb-6 text-center sm:text-left">
-              <span className="text-sm text-[#276DF7]">Patients </span> / View
-              Patients
-            </h2>
+            {renderBreadcrumb()}
             <div className="flex justify-center sm:justify-end w-auto">
               <CustomButton
                 title="Assign Package To Patient"
                 containerStyles="bg-[#1F5AF4] text-white px-6 py-2 mb-4"
+                handleClick={() => setShowAssignPackage(!showAssignPackage)}
               />
             </div>
           </div>
 
-          <div className="flex flex-col lg:flex-row min-h-screen mt-8">
-            <aside className="w-full lg:w-64 bg-white mb-4 lg:mb-0">
-              <nav className="space-y-4">
-                <p className="text-gray-500">Patient</p>
-                <button className="w-full text-left font-semibold text-blue-600 p-4 bg-[#276DF71A] border-r-4 border-[#276DF7]">
-                  Rider&apos;s Profile
-                </button>
-                <button className="w-full text-left text-gray-700 hover:text-blue-600 p-2 rounded-lg">
-                  Delivery History
-                </button>
-              </nav>
-            </aside>
+          {showAssignPackage ? (
+            <AssignPackage />
+          ) : (
+            <div className="flex flex-col lg:flex-row min-h-screen mt-8 ">
+              <aside className="w-full lg:w-64 bg-white mb-4 lg:mb-0 p-4 h-[210px]">
+                <nav className="space-y-4">
+                  <p className="text-gray-500">Patient</p>
+                  <button className="w-full text-left font-semibold text-blue-600 p-4 bg-[#276DF71A] border-r-4 border-[#276DF7]">
+                    Rider&apos;s Profile
+                  </button>
+                  <button className="w-full text-left text-gray-700 hover:text-blue-600 p-2 rounded-lg">
+                    Delivery History
+                  </button>
+                </nav>
+              </aside>
 
-            <div className="flex-1 bg-white lg:ml-48 px-4 lg:px-0">
-              <div className="flex flex-col lg:flex-row justify-between border-b pb-2 items-center space-y-4 lg:space-y-0">
-                <div className="flex items-center space-x-4">
-                  <p>Payment Status</p>
-                  <span
-                    className={`px-4 py-1 font-medium rounded-lg ${getStatusColor(
-                      patient.status
-                    )}`}
-                  >
-                    {patient.status}
-                  </span>
+              <div className="flex-1 bg-white lg:ml-48 px-4 lg:px-0 h-[600px] ">
+                <div className="flex flex-col lg:flex-row justify-between border-b pb-2 items-center space-y-4 lg:space-y-0">
+                  <div className="flex items-center space-x-4 p-4">
+                    <p>Payment Status</p>
+                    <span
+                      className={`px-4 py-1 font-medium rounded-lg ${getStatusColor(
+                        patient.status
+                      )}`}
+                    >
+                      {patient.status}
+                    </span>
+                  </div>
+
+                  <div className="space-x-6 ">
+                    <button
+                      className={`pb-4 font-semibold ${
+                        activeTab === "patient"
+                          ? "text-blue-600 border-b-2 border-blue-600"
+                          : "text-gray-500 hover:text-blue-600"
+                      }`}
+                      onClick={() => setActiveTab("patient")}
+                    >
+                      Patient Information
+                    </button>
+                    <button
+                      className={`pb-4 font-semibold p-4  ${
+                        activeTab === "delivery"
+                          ? "text-blue-600 border-b-2 border-blue-600"
+                          : "text-gray-500 hover:text-blue-600"
+                      }`}
+                      onClick={() => setActiveTab("delivery")}
+                    >
+                      Delivery Information
+                    </button>
+                  </div>
                 </div>
 
-                <div className="space-x-6">
-                  <button className="text-blue-600 border-b-2 border-blue-600 pb-4 font-semibold">
-                    Patient Information
-                  </button>
-                  <button className="text-gray-500 hover:text-blue-600 pb-2">
-                    Delivery Information
-                  </button>
-                </div>
-              </div>
+                <div className="mt-10 flex flex-col lg:flex-row justify-between gap-6 lg:gap-0">
+                  <div className="items-center">
+                    <p className="text-lg p-4 font-semibold text-gray-900">
+                      {activeTab === "patient"
+                        ? "Patient Information"
+                        : "Delivery Information"}
+                    </p>
+                    <p className="mt-4 p-4">
+                      {activeTab === "patient"
+                        ? "Personal information about Patient."
+                        : "Delivery details and tracking information."}
+                    </p>
+                    <CustomButton
+                      title={
+                        activeTab === "patient"
+                          ? "Edit Patient's Information"
+                          : "Edit Delivery Information"
+                      }
+                      containerStyles="border border-[#1F5AF4] text-[#1F5AF4] px-6 py-2  ml-4 mb-4 mt-4 font-bold"
+                    />
+                  </div>
 
-              <div className="mt-10 flex flex-col lg:flex-row justify-between gap-6 lg:gap-0">
-                <div className="items-center">
-                  <p className="text-lg font-semibold text-gray-900">
-                    Patient Information
-                  </p>
-                  <p className="mt-4">Personal information about Patient.</p>
+                  {activeTab === "patient"
+                    ? renderPatientForm()
+                    : renderDeliveryForm()}
+                </div>
+
+                <div className="mt-6 flex justify-end">
                   <CustomButton
-                    title="Edit Patient's Information"
-                    containerStyles="border border-[#1F5AF4] text-[#1F5AF4] px-6 py-2 mb-4 mt-4 font-bold"
+                    title="Save Changes"
+                    containerStyles="bg-[#1F5AF4] px-6 py-2 mb-4 mt-4 font-bold text-white"
                   />
                 </div>
-
-                <form className="w-full lg:w-2/3 grid grid-cols-1 gap-6">
-                  <div className="border-[2px] border-[#CFCFCF] bg-[#EFEFEF] p-2">
-                    <label className="text-sm">Hospital ID</label>
-                    <input
-                      type="text"
-                      className="w-full border-none outline-none mt-1 bg-transparent font-gilroy font-bold"
-                      value={patient.hospital_id}
-                      readOnly
-                    />
-                  </div>
-
-                  <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-                    <div className="border-[2px] border-[#CFCFCF] bg-[#EFEFEF] p-2">
-                      <label className="text-sm text-gray-500">
-                        First Name
-                      </label>
-                      <input
-                        type="text"
-                        className="w-full border-none outline-none mt-1 bg-transparent font-gilroy font-bold"
-                        value={firstName}
-                        onChange={(e) => {
-                          setFirstName(e.target.value);
-                          if (patient) {
-                            const updatedPatient = {
-                              ...patient,
-                              patient_name:
-                                `${e.target.value} ${lastName}`.trim(),
-                            };
-                            setPatient(updatedPatient);
-                          }
-                        }}
-                      />
-                    </div>
-
-                    <div className="border-[2px] border-[#CFCFCF] bg-[#EFEFEF] p-2">
-                      <label className="text-sm text-gray-500">Last Name</label>
-                      <input
-                        type="text"
-                        className="w-full border-none outline-none mt-1 bg-transparent font-gilroy font-bold"
-                        value={lastName}
-                        onChange={(e) => {
-                          setLastName(e.target.value);
-                          if (patient) {
-                            const updatedPatient = {
-                              ...patient,
-                              patient_name:
-                                `${firstName} ${e.target.value}`.trim(),
-                            };
-                            setPatient(updatedPatient);
-                          }
-                        }}
-                      />
-                    </div>
-                  </div>
-
-                  <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-                    <div className="border-[2px] border-[#CFCFCF] bg-[#EFEFEF] p-2">
-                      <label className="text-sm text-gray-500">Gender</label>
-                      <select className="w-full border-none outline-none mt-1 bg-transparent font-gilroy font-bold">
-                        <option
-                          value="Male"
-                          selected={patient.gender === "Male"}
-                        >
-                          Male
-                        </option>
-                        <option
-                          value="Female"
-                          selected={patient.gender === "Female"}
-                        >
-                          Female
-                        </option>
-                      </select>
-                    </div>
-
-                    <div className="border-[2px] border-[#CFCFCF] bg-[#EFEFEF] p-2">
-                      <label className="text-sm text-gray-500">
-                        Phone Number
-                      </label>
-                      <input
-                        type="text"
-                        className="w-full border-none outline-none mt-1 bg-transparent font-gilroy font-bold"
-                        value={patient.phone_number}
-                      />
-                    </div>
-                  </div>
-
-                  <div className="border-[2px] border-[#CFCFCF] bg-[#EFEFEF] p-2">
-                    <label className="text-sm text-gray-500">
-                      Email Address
-                    </label>
-                    <input
-                      type="email"
-                      className="w-full border-none outline-none mt-1 bg-transparent font-gilroy font-bold"
-                      value={patient.email}
-                    />
-                  </div>
-                </form>
-              </div>
-
-              <div className="mt-6 flex justify-end">
-                <CustomButton
-                  title="Save Changes"
-                  containerStyles="bg-[#1F5AF4] px-6 py-2 mb-4 mt-4 font-bold text-white"
-                />
               </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
